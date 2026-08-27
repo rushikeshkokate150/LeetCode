c33f9862de3b50234f5c4719db0e487cca38d816
Rushikesh Kokate <rushikeshkokate150@gmail.com>
2026-08-27 16:22:25 +0530

    leetcode easy

---
+++ removed linkedlist element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+
+        if head is None:
+            return None
+
+        head.next = self.removeElements(head.next, val)
+
+        if head.val == val:
+            return head.next
+
+        return head
+
+
